f34b7749404868ceba967b0c4aa86ee57f6a20bf
JeroC03 <jeroccasanova@gmail.com>
2023-06-15 23:07:05 -0300

    tp2

--- WEB/Flask_proyecto/app.py
+++ WEB/Flask_proyecto/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from markupsafe import escape
 
 prendas = [
@@ -16,10 +16,27 @@ def home():
 def get_all_prendas():
     return render_template("prendas.html", prendas=prendas.items())
 
-@app.get("/prendas/<id>")
+@app.get("/prendas/<int:id>")
 def get_prenda(id):
     if id in prendas:
         prenda = prendas[id]
         return render_template("prenda.html", id=id, prenda=prenda)
     else:
         return ("no hay prenda", 404)
+    
+@app.get("/ventas/<int:id>")
+def get_ventas(id):
+    return {"foo":1}
+
+@app.route("/success/")
+def success():
+    return render_template("success.html")
+
+@app.route("/prendas/new/", methods=('GET', 'POST'))
+def create_prenda():
+    if request.method == 'POST':
+        valor_input = request.form.get("name").split(",")
+        prendas[int(valor_input[0])] = {"name": valor_input[1], "price": valor_input[2]}
+        return redirect(url_for("success"))
+    else:
+        return render_template("new_prendas.html")
