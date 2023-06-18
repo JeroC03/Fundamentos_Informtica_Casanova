408531f06af438c7db6b7cd714995d4eefb4b0af
JeroC03 <jeroccasanova@gmail.com>
2023-06-18 14:12:42 -0300

    tp3

---
+++ WEB/tp3/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+from markupsafe import escape
+from flask_wtf.csrf import generate_csrf
+
+app = Flask(__name__) 
+
+productos = {
+    1:{"numero": 1, "Producto": "Shampoo sólido", "Stock": 5, "Precio_unitario": 300},
+    2:{"numero": 2, "Producto": "Crema de manos", "Stock": 6, "Precio_unitario": 600},
+    3:{"numero": 3, "Producto": "Crema de pies", "Stock": 2, "Precio_unitario": 100}
+}
+
+@app.get("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/productos", methods=["GET"])
+def obtener_productos():
+    return render_template("productos.html", productos=productos.values())   
+
+@app.route("/productos/<int:numero>")
+def obtener_producto(numero):
+    if numero in productos:
+        producto = productos[numero]
+        return render_template("producto.html", producto=producto)
+    else:
+        return jsonify({"mensaje": "Producto no encontrado"}), 404
+    
